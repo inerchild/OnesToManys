18dc86fe560a14a98b4f28573cdea9db5a407909
Antoinette <inerchild2000@gmail.com>
2025-12-04 15:25:02 -0500

    "initialize spring"

---
+++ residential-communities/src/main/java/com/zipcode/residential_communities/ResidentialCommunitiesApplication.java
@@ -0,0 +1,13 @@
+package com.zipcode.residential_communities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidentialCommunitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidentialCommunitiesApplication.class, args);
+	}
+
+}
---
+++ residential-communities/src/test/java/com/zipcode/residential_communities/ResidentialCommunitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcode.residential_communities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentialCommunitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
